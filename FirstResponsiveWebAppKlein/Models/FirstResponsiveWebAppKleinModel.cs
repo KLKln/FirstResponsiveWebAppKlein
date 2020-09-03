75a150b77f462ca6aea855c61990fce047f10ab9
Kelly Klein <kmklein2@dmacc.edu>
2020-09-02 21:12:12 -0500

    Update FirstResponsiveWebAppKleinModel.cs

--- FirstResponsiveWebAppKlein/Models/FirstResponsiveWebAppKleinModel.cs
+++ FirstResponsiveWebAppKlein/Models/FirstResponsiveWebAppKleinModel.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/**************************************************************
+* Name        : FirstResponsiveWebAppKlein.cs
+* Author      : Kelly Klein
+* Created     : 09/02/2020
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : Program takes user input and outputs name and birthday(due to misreading of instructions)
+*               Input:  Name: takes name of user; Birthday: takes birthday of user through a dropdown menu
+*               Output: returns a greeting sepeating back users name and birthday
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
