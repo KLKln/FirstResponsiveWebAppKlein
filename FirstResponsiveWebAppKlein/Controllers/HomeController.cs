6a44ae906ad7c0d2ac753bf0722fb1e01deac700
Kelly Klein <kmklein2@dmacc.edu>
2020-09-02 21:07:09 -0500

    Initial commit

---
+++ FirstResponsiveWebAppKlein/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppKlein.Models;
+
+namespace FirstResponsiveWebAppKlein.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Name = "";
+        public String BD = "";
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "";
+            ViewBag.BD = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FirstResponsiveWebAppKleinModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.BD = model.Greeting();
+            }
+            else
+            {
+                ViewBag.BD = "";
+            }
+            return View(model);
+
+        }
+
+    }
+}
---
+++ FirstResponsiveWebAppKlein/Models/FirstResponsiveWebAppKleinModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppKlein.Models
+{
+    public class FirstResponsiveWebAppKleinModel
+    {
+        public string ErrorMessage = "";
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your birthday.")]
+        public DateTime BirthYear { get; set; }
+        public string userName = "";
+        public string userBirthday = "";
+
+        public String Greeting()
+        {
+            if (Regex.IsMatch(Name, @"^[\p{L}]+$"))
+            {
+                string userName = Name;
+            }
+            else
+            {
+                ErrorMessage = "Alphabet only for name please";
+            }
+            DateTime birthday;
+            if (DateTime.TryParseExact(BirthYear.ToString(), "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out birthday))
+            {
+                string userBirthday = birthday.ToString();
+            }
+            else
+            {
+                ErrorMessage = "Date only in this format: 'MM/dd/yyyy'.";
+            }
+            string greeting = "Hello " + Name + ", your birthday is " + BirthYear.ToString() + ".";
+            return greeting;
+        }
+    }
+
+}
